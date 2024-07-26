233b2578bfdd2a1a0c101359896b887c561fc692
Rustam2900 <139065064+Rustam2900@users.noreply.github.com>
2024-07-27 01:31:39 +1000

    add (bot(admin) and webapp(admin))

--- bot/admin.py
+++ bot/admin.py
@@ -1,1 +1,27 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
+from bot.models import BotUser, BotCompany, BotUserOrder, BotCompanyOrder
+
+
+@admin.register(BotUser)
+class BotUserAdmin(TranslationAdmin):
+    list_display = ['id', 'name', 'contact', 'add_contact']
+    search_fields = ['name', 'contact', 'contact']
+
+
+@admin.register(BotCompany)
+class BotCompanyAdmin(TranslationAdmin):
+    list_display = ['id', 'company_name', 'company_employee_name', 'company_contact']
+    search_fields = ['company_name', 'company_employee_name', 'company_contact']
+
+
+@admin.register(BotCompanyOrder)
+class BotCompanyOrderAdmin(TranslationAdmin):
+    list_display = ['id', 'product_name', 'quantity', 'total_price', 'create_at']
+    search_fields = ['id', 'product_name']
+
+
+@admin.register(BotUserOrder)
+class BotUserOrderAdmin(TranslationAdmin):
+    list_display = ['id', 'product_name', 'quantity', 'total_price', 'create_at']
+    search_fields = ['id', 'product_name']
--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from webapp.models import Product, CartItem
+from modeltranslation.admin import TranslationAdmin
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(TranslationAdmin):
+    list_display = ('id', 'name', 'image', 'price', 'description')
+    search_fields = ('id', 'name')
+    list_filter = ('id', 'name', 'price', 'description')
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'quantity', 'total_price')
+    search_fields = ('id', 'product', 'quantity')
+    list_filter = ('id', 'product', 'quantity', 'total_price')
